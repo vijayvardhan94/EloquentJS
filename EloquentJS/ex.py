3173bc551a96150ce10d3c42da21d08e07fe63ae
Vijay Tadimeti <tadimetv@oregonstate.edu>
2020-01-22 10:08:49 -0800

    warm

---
+++ EloquentJS/ex.py
@@ -0,0 +1,10 @@
+#for i in range(1,10):
+    #if i == 5:
+        #break
+    #print (i)
+
+#alist = ['a', 'b', 'c', 'd', 'e', 'f']
+#for i in alist:
+   # if alist[i] == 3:
+    #    break
+     #   print(i)
